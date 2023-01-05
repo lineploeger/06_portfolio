16a1e6da776b923f69c99672aeda7a80b6db14d6
Line Ploger <lilleploger@gmail.com>
2023-01-05 12:20:52 +0100

    first commit

---
+++ javascript/burgermenu.js
@@ -0,0 +1,11 @@
+// Lav variablen "btn", der henviser til ".toggle-btn"
+const btn = document.querySelector(".toggle-btn");
+// Lav variablen "menu", der henviser til ".main-menu"
+const menu = document.querySelector(".main-menu");
+
+// Lav funktionen "toggleMenu()" med følgende funktionalitet
+function toggleMenu() {
+  menu.classList.toggle("kryds");
+}
+
+btn.addEventListener("click", toggleMenu);
